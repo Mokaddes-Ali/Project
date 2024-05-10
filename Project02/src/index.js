b3331378ee6ebfcf21a95f2ede02ee638fde2c0f
mokaddes <mokaddes.ru2000@gmail.com>
2024-05-10 19:04:30 +0600

    commit

---
+++ Project02/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import reportWebVitals from './reportWebVitals';
+import NavBar from './Components/NavBar/NavBar';
+import TopHeader from './Components/TopHeader/TopHeader';
+import Innerheader from './Components/Innerheader/Innerheader';
+import MeetingSection from './Components/MeetingSection/MeetingSection';
+import ChartBoxSection from './Components/ChartBoxSection/ChartBoxSection';
+import OrderSection from './Components/OrderSection/OrderSection';
+import ReviewSection from './Components/ReviewSection/ReviewSection';
+import Footer from './Components/Footer/Footer';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+     <NavBar />
+   <TopHeader />
+   <Innerheader />
+   <MeetingSection />
+   <ChartBoxSection />
+   <OrderSection />
+   <ReviewSection />
+   <Footer />
+  </React.StrictMode>
+);
+
+reportWebVitals();
